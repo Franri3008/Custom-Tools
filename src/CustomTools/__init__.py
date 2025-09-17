e5b3ca26ea5e991a69708946e6dd8883f9039f8f
Franri <franrios9930@gmail.com>
2025-09-17 10:43:13 +0200

    fix: import bug

--- src/CustomTools/__init__.py
+++ src/CustomTools/__init__.py
@@ -1,4 +1,8 @@
-from . import data;
-from . import llm;
+import importlib
 
-__all__ = ["data", "llm"];
+__all__ = ["llm"];
+
+def __getattr__(name):
+    if name in __all__:
+        return importlib.import_module(f".{name}", __name__);
+    raise AttributeError(f"module {__name__} has no attribute {name}");
